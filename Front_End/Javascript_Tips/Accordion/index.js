e3741d1e575982aee1a552218f6e5d02a03b4bce
Rajshekarreddy78 <rajshekarreddyr07@gmail.com>
2023-04-11 20:41:27 +0530

    Acc & Flip

--- Front_End/Javascript_Tips/Accordion/index.js
+++ Front_End/Javascript_Tips/Accordion/index.js
@@ -1,8 +1,9 @@
 const tabButtons = document.querySelectorAll('.tab__button');
         
+
         tabButtons.forEach((e) => {
             e.addEventListener("click", (e) => {
-                if (e.target.classList.contains('tab__button-active')) {
+                if (!e.target.classList.contains('tab__button-active')) {
                     e.target.classList.add("tab__button-active");
                     e.target.nextElementSibling.style.height = e.target.nextElementSibling.firstElementChild.clientHeight + 60 + "px";
                     e.target.nextElementSibling.classList.add("tab__panel-active");
